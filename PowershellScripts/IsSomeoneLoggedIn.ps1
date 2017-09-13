d5302c7b4e9df1d89734c74d743547ce103a84cf
Mo01 <moh.alneama@yahoo.com>
2017-09-13 14:25:54 -0500

    Added parameters to scripts.

--- PowershellScripts/IsSomeoneLoggedIn.ps1
+++ PowershellScripts/IsSomeoneLoggedIn.ps1
@@ -1,4 +1,12 @@
-$result = Invoke-Command -Computername QA_PowerUltra -Credential QA_PowerUltra\Administrator -Scriptblock {quser}
+Param(
+  [string]$computer,
+  [string]$password
+)
+
+$secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ($computer + "\Administrator", $secpasswd)
+
+$result = Invoke-Command -Computername QA_PowerUltra -Credential $mycreds -Scriptblock {quser}
 if ($result -match "Active")
 {
     $true
