1c263ff23a9a713cadefe92ae45fbb62f338dbb2
Jon Wesneski <jon.wesneski@salientsys.com>
2017-10-19 13:24:12 -0500

    created a batch file to copy files over automatically

---
+++ CopyToShared.bat
@@ -0,0 +1,4 @@
+start /w robocopy . \\valhalla\shared\QA\Automation\Roctopus\ Roctopus.hta /it /is
+start /w robocopy .\JS \\valhalla\shared\QA\Automation\Roctopus\JS /it /is
+start /w robocopy .\CSS \\valhalla\shared\QA\Automation\Roctopus\CSS /it /is
+start /w robocopy .\PowershellScripts \\valhalla\shared\QA\Automation\Roctopus\PowershellScripts /it /is
